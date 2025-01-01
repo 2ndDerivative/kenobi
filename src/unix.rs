61f074b942deff88a745b3fd97707eb65c4a877a
2ndDerivative <niclas@klugmann.de>
2025-01-01 21:27:52 +0100

    add equivalent method stubs to unix contexts

--- src/unix.rs
+++ src/unix.rs
@@ -1,18 +1,35 @@
-use crate::StepResult;
+use std::ffi::OsString;
+
+use crate::{SecurityInfo, StepResult};
 
 pub struct SecurityInfoHandle;
-#[derive(Debug)]
-pub struct FinishedContext;
+pub struct ContextBuilder;
+impl ContextBuilder {
+    pub(crate) fn step_impl(self, _token: &[u8]) -> StepResult {
+        unimplemented!()
+    }
+}
 #[derive(Debug)]
 pub struct PendingContext;
+impl SecurityInfo for PendingContext {
+    fn security_info(&self) -> SecurityInfoHandle {
+        SecurityInfoHandle
+    }
+}
 impl PendingContext {
     pub(crate) fn step_impl(self, _token: &[u8]) -> StepResult {
         unimplemented!()
     }
 }
-pub struct ContextBuilder;
-impl ContextBuilder {
-    pub(crate) fn step_impl(self, _token: &[u8]) -> StepResult {
+#[derive(Debug)]
+pub struct FinishedContext;
+impl FinishedContext {
+    pub fn client_target(&self) -> Result<OsString, String> {
         unimplemented!()
+    }
+}
+impl SecurityInfo for FinishedContext {
+    fn security_info(&self) -> SecurityInfoHandle {
+        SecurityInfoHandle
     }
 }
