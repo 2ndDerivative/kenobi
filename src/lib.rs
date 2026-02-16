2179446607d6c8927098b4543d4f147f69eee6d8
Niclas Klugmann <klugmann@heggemann.com>
2026-02-16 17:23:02 +0100

    expose error for Credentials acquisition

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,23 @@ use kenobi_windows::credentials::Credentials as WinCred;
 
 pub mod client;
 
+#[derive(Debug)]
+pub struct CredentialsError {
+    #[cfg(windows)]
+    win: kenobi_windows::credentials::Error,
+    #[cfg(unix)]
+    unix: kenobi_unix::Error,
+}
+impl std::error::Error for CredentialsError {}
+impl std::fmt::Display for CredentialsError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        #[cfg(windows)]
+        return self.win.fmt(f);
+        #[cfg(unix)]
+        self.unix.fmt(f)
+    }
+}
+
 /// A GSSAPI credentials handle
 pub struct Credentials {
     #[cfg(windows)]
@@ -21,16 +38,15 @@ impl Credentials {
     fn into_platform(self) -> UnixCred {
         self.inner
     }
-    #[must_use]
     /// Grab the default credentials handle for a given principal (or the default user principal)
     ///
     /// On windows, this will use the current security context, and on Unix, this will use the default Keytab/ticket store
-    pub fn acquire_default(usage: CredentialsUsage, principal: Option<&str>) -> Self {
+    pub fn acquire_default(usage: CredentialsUsage, principal: Option<&str>) -> Result<Self, CredentialsError> {
         #[cfg(windows)]
-        let inner = WinCred::acquire_default(usage.to_windows(), principal);
+        let inner = WinCred::acquire_default(usage.to_windows(), principal).map_err(|win| CredentialsError { win })?;
         #[cfg(unix)]
-        let inner = UnixCred::acquire_default(usage.to_unix(), principal).unwrap();
-        Self { inner }
+        let inner = UnixCred::acquire_default(usage.to_unix(), principal).map_err(|unix| CredentialsError { unix })?;
+        Ok(Self { inner })
     }
 }
 
