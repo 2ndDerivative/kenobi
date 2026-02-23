fd6e1fef25da12f2ec4d730954e62a117402521c
Niclas Klugmann <niclas@klugmann.de>
2026-02-23 21:39:58 +0100

    dont require KERBEROS_TEST_SERVICE_PRINCIPAL

--- crates/kenobi-unix/tests/acquire_creds.rs
+++ crates/kenobi-unix/tests/acquire_creds.rs
@@ -6,7 +6,7 @@ use kenobi_unix::{
 #[test]
 fn main() {
     let client_name = std::env::var("KERBEROS_TEST_USER_PRINCIPAL").ok();
-    let service_principal = std::env::var("KERBEROS_TEST_SERVICE_PRINCIPAL").unwrap();
+    let service_principal = std::env::var("KERBEROS_TEST_SERVICE_PRINCIPAL").ok();
     let cred = match Credentials::outbound(client_name.as_deref(), None) {
         Ok(cred) => cred,
         Err(err) => {
@@ -14,7 +14,7 @@ fn main() {
             panic!()
         }
     };
-    let mut _ctx = match ClientContext::new(cred, Some(service_principal.as_str())) {
+    let mut _ctx = match ClientContext::new(cred, service_principal.as_deref()) {
         Ok(StepOut::Finished(_)) => return,
         Ok(StepOut::Pending(pending)) => pending,
         Err(err) => {
