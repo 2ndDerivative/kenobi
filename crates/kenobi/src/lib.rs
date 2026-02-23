371c6d57ac944c955b8ad6c8152722d78c10fa01
Niclas Klugmann <niclas@klugmann.de>
2026-02-23 22:18:51 +0100

    export CredentialsUsage at cred::

--- crates/kenobi/src/lib.rs
+++ crates/kenobi/src/lib.rs
@@ -2,18 +2,18 @@ use std::marker::PhantomData;
 
 use kenobi_core::cred::usage::{Outbound, OutboundUsable};
 #[cfg(unix)]
-use kenobi_unix::cred::Credentials as UnixCred;
-#[cfg(unix)]
-pub use kenobi_unix::cred::CredentialsUsage;
+use kenobi_unix::cred::{Credentials as UnixCred, CredentialsUsage};
 #[cfg(windows)]
-use kenobi_windows::cred::Credentials as WinCred;
-#[cfg(windows)]
-pub use kenobi_windows::cred::CredentialsUsage;
+use kenobi_windows::cred::{Credentials as WinCred, CredentialsUsage};
 pub mod client;
 mod sign_encrypt;
 
 pub mod cred {
     pub use kenobi_core::cred::usage::{Both, Inbound, InboundUsable, Outbound, OutboundUsable};
+    #[cfg(unix)]
+    pub use kenobi_unix::cred::CredentialsUsage;
+    #[cfg(windows)]
+    pub use kenobi_windows::cred::CredentialsUsage;
 }
 pub mod channel_bindings {
     pub use kenobi_core::channel_bindings::Channel;
