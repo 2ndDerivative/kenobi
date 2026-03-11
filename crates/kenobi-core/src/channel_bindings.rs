c133da1b22444df08de017f254abbdf88a44162d
Niclas Klugmann <niclas@klugmann.de>
2026-03-11 03:55:17 +0100

    FIX FUCKING CHANNEL BINDINGS

--- crates/kenobi-core/src/channel_bindings.rs
+++ crates/kenobi-core/src/channel_bindings.rs
@@ -8,6 +8,15 @@ impl<S: std::io::Read + std::io::Write> Channel for native_tls::TlsStream<S> {
     type Error = native_tls::Error;
 
     fn channel_bindings(&self) -> Result<Option<Vec<u8>>, Self::Error> {
-        self.tls_server_end_point()
+        match self.tls_server_end_point() {
+            Ok(Some(v)) => {
+                const PREFIX: &'static [u8] = b"tls-server-end-point:";
+                let mut vec = Vec::with_capacity(PREFIX.len() + v.len());
+                vec.extend_from_slice(PREFIX);
+                vec.extend_from_slice(&v);
+                Ok(Some(vec))
+            }
+            other => other,
+        }
     }
 }
